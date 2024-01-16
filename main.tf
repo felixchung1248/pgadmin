48e0c9e2a9b1f5ac5d07ef8d5d59708e0bfe423c
Felix <felix.chung@felixchung.net>
2024-01-16 18:31:01 +0800

    change app name

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
-resource "helm_release" "pgadmin" {
-  name       = "pgadmin"
+resource "helm_release" "pgadmin4" {
+  name       = "pgadmin4"
   chart      = "/chart"
   namespace  = "postgresql-ns"
   create_namespace = true
